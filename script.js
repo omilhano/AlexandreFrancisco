f9bad1ecfa54bb972a6cebd384a5235d80242523
Alexandre Francisco <alexmvfrancisco@gmail.com>
2024-05-10 00:26:32 +0100

    landing page almost finshed
    just change nav links color when hovered

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("bi-github").onclick = function (){
+    location.href = "https://github.com/omilhano";
+}
+
+document.getElementById("bi-linkedin").onclick = function(){
+    location.href = "https://www.linkedin.com/in/alexandremvfrancisco/"
+}
+
+
+
