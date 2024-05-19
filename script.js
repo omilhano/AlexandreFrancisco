7487c30de999bf4fb2bea0253d16bb3457e56123
omilhano <alexmvfrancisco@gmail.com>
2024-05-19 23:33:21 +0100

    updated landing page
    mixed colors for better readability

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+// functionality to button
 document.getElementById("bi-github").onclick = function (){
     location.href = "https://github.com/omilhano";
 }
 
+// functionality to button
 document.getElementById("bi-linkedin").onclick = function(){
     location.href = "https://www.linkedin.com/in/alexandremvfrancisco/"
 }
 
+// ability to see in which section of the html document user is in
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    //add active to nav-link class
+    function setActiveLink() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    }
+
+    setActiveLink();
+    window.addEventListener('scroll', setActiveLink);
+});
