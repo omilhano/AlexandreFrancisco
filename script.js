2416bd1bdd5b3a4850d4821c01da30c1a82910cb
omilhano <alexmvfrancisco@gmail.com>
2024-09-16 00:17:14 +0100

    projects started yipee

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // functionality to button
-document.getElementById("bi-github").onclick = function (){
+document.getElementById("bi-github").onclick = function () {
     location.href = "https://github.com/omilhano";
 }
 
 // functionality to button
-document.getElementById("bi-linkedin").onclick = function(){
+document.getElementById("bi-linkedin").onclick = function () {
     location.href = "https://www.linkedin.com/in/alexandremvfrancisco/"
 }
 
 // ability to see in which section of the html document user is in
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('.nav-link');
 
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function setActiveLink() {
         let index = sections.length;
 
-        while(--index && window.scrollY + 53 < sections[index].offsetTop) {}
+        while (--index && window.scrollY + 53 < sections[index].offsetTop) { }
 
         navLinks.forEach((link) => link.classList.remove('active'));
         navLinks[index].classList.add('active');
@@ -25,4 +25,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setActiveLink();
     window.addEventListener('scroll', setActiveLink);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const projectItem = document.querySelector('.project-item');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            }
+        });
+    }, { threshold: 0.5});
+
+    observer.observe(projectItem);
 });
