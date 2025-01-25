75fd13a34f07d3dc3219bb64f3436c11ba1b270c
omilhano <alexmvfrancisco@gmail.com>
2025-01-25 16:37:04 +0000

    Testing updates

--- script.js
+++ script.js
@@ -7,36 +7,3 @@ document.getElementById("bi-github").onclick = function () {
 document.getElementById("bi-linkedin").onclick = function () {
     location.href = "https://www.linkedin.com/in/alexandremvfrancisco/"
 }
-
-// ability to see in which section of the html document user is in
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('.nav-link');
-
-    //add active to nav-link class
-    function setActiveLink() {
-        let index = sections.length;
-
-        while (--index && window.scrollY + 53 < sections[index].offsetTop) { }
-
-        navLinks.forEach((link) => link.classList.remove('active'));
-        navLinks[index].classList.add('active');
-    }
-
-    setActiveLink();
-    window.addEventListener('scroll', setActiveLink);
-});
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const projectItem = document.querySelector('.project-item');
-
-//     const observer = new IntersectionObserver((entries) => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add('in-view');
-//             }
-//         });
-//     }, { threshold: 0.5});
-
-//     observer.observe(projectItem);
-// });
